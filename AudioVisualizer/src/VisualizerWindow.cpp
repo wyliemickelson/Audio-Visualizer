7927b6f9c763bca2be7295299a54e2cf55adad73
Ashley S. <foxhound118451@gmail.com>
2025-11-20 15:09:39 -0800

    Add GLAD, Render function outline, update .gitignore.

--- AudioVisualizer/src/VisualizerWindow.cpp
+++ AudioVisualizer/src/VisualizerWindow.cpp
@@ -1,19 +1,43 @@
+#include <glad/glad.h>
 #include <VisualizerWindow.h>
 
-wxGLAttributes glAttributes;
-
-VisualizerWindow::VisualizerWindow(wxWindow* parent, const wxGLAttributes glAttributes) :  wxGLCanvas(parent, glAttributes)
+VisualizerWindow::VisualizerWindow(wxWindow* parent, const wxGLAttributes display_attribs) :  wxGLCanvas(parent, display_attribs)
 {
-	wxGLContext glContext = wxGLContext(this);
+	wxGLContextAttrs glAttributes;
+	glAttributes.CoreProfile().OGLVersion(3, 3).EndList();
+	wxGLContext glContext = wxGLContext(this, nullptr, &glAttributes);
 	SetCurrent(glContext);
 }
 
 /*
 * Enter processed audio data, draw using OpenGL
+*
+* Each bucket is represented by a circle.
+* Position is left/right channel presence,
+* Size is loudness,
+* Color separates different frequency buckets.
 */
-void VisualizerWindow::Render(BYTE data)
+void VisualizerWindow::Render(FreqData data)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	//vertices defining a diamond shape
+	float diamond[] = {
+		-1.0f, 0.5f,
+		0.0f, 0.0f,
+		1.0f, 0.5f,
+		0.0f, 1.0f
+	};
+	
+	unsigned int VAO;
+	unsigned int VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), diamond, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	SwapBuffers();
 }
--- AudioVisualizer/src/VisualizerWindow.h
+++ AudioVisualizer/src/VisualizerWindow.h
@@ -1,12 +1,19 @@
 #include <wx/glcanvas.h>
 #include <wx/frame.h>
 
+struct FreqData 
+{
+	float stereo_pos; //from 0-100, represents left-right pos
+	float size; //size multiplier
+	float color; //rgb
+};
+
 /*
 * wxWidgets window using OpenGL to draw audio visualizations 
 */
 class VisualizerWindow : public wxGLCanvas
 {
 public:
-	VisualizerWindow(wxWindow* parent, wxGLAttributes glAttributes);
-	void Render(BYTE data);
+	VisualizerWindow(wxWindow* parent, wxGLAttributes display_attributes);
+	void Render(FreqData data);
 };
